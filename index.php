3f2e0ea62a828f4b81c14e0d2859fddbfd68cadc
unknown <apost906@wit-xdvm-071.MILLARDPS.NET>
2012-12-19 11:24:15 -0600

    Posts can be viewed

--- index.php
+++ index.php
@@ -12,27 +12,43 @@
 				<?php get_template_part('nav') ?>
 			</nav>
 			<div id="main">
-				<?php 
-				/**
-				 * Check to see if the current request
-				 * is_category()
-				 * is_front_page()
-				 * is_home()
-				 * is_404()
-				 * is_archive()
-				 * is_page()
-				 * is_single() --> e.g.
-				 * is_tag()
-				 * is_search() --> a page display search results
-				 */
-				if(is_home())	{
-					// Includes file named content-home.php
-					get_template_part('content','home');
-				}	elseif (is_page() || is_single())	{
-					// Includes file named content-single.php
-					get_template_part('content','single');
-				}
-				?>
+				<div class="row">
+					<!-- MAIN COLUMN -->
+					<div class="span9">
+						<?php 
+						/**
+						 * Check to see if the current request
+						 * is_category()
+						 * is_front_page()
+						 * is_home()
+						 * is_404()
+						 * is_archive()
+						 * is_page()
+						 * is_single() --> e.g.
+						 * is_tag()
+						 * is_search() --> a page display search results
+						 */
+						if(is_front_page())	{
+							// Includes file named content-home.php
+							get_template_part('content','home');
+						}	elseif (is_page() || is_single())	{
+							// Includes file named content-single.php
+							get_template_part('content','single');
+						}	elseif (is_search())	{
+							// Includes file named content-search.php
+							get_template_part('content','search');
+						}	else	{
+							// Includes file named content-blog.php
+							get_template_part('content','blog');
+						}
+						?>
+					</div>
+					<!-- RIGHT SIDEBAR -->
+					<div class="span3">
+						<?php get_sidebar() ?>
+					</div>
+				</div>
+				
 			</div>
 			<footer>
 				<?php get_footer(); ?>
